7e72d969906ba7b8cd017834c76f21247f51e52d
cjp <chenjm314@163.com>
2019-04-19 17:20:11 +0800

    用netty接收日志

--- src/main/java/com/hq/tool/TPLogHandle.java
+++ src/main/java/com/hq/tool/TPLogHandle.java
@@ -21,7 +21,9 @@ public class TPLogHandle {
             //解析  IPCP协商成功，本地地址:116.24.101.77, 对端地址:116.24.100.1, DNS1:202.96.134.33, DNS2:202.96.128.86
             int ipcpIndex = tpLog.indexOf(IPCP_FLAG);
             if(ipcpIndex > -1){
+                log.info("日志中包含关键字:{}",IPCP_FLAG);
                 String ip =analysisIp(ipcpIndex,tpLog);
+                log.info("分析日志检测到ip发生变化，新ip{}，开始重新解析域名。。。",ip);
                 aliSDKHelp.updateDomain(ip);
             }
 
@@ -29,7 +31,7 @@ public class TPLogHandle {
     }
 
     protected   String analysisIp(int ipcpIndex,String tpLog){
-
+        log.info("日志中解析ip。。。");
         String str = tpLog.substring(ipcpIndex+IPCP_FLAG_LENGTH);
         String ip = str.split(IP_SPLIT_FLAG,2)[0];
         return ip;
